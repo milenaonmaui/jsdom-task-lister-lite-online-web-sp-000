32298e7549c9d45f9769e28cd6b555ee5a20638b
Milena Kari <milenakari@yahoo.com>
2019-10-18 23:21:08 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.getElementById("submit").addEventListener("click", function(event){
+    event.preventDefault();
+    let ul = document.getElementById("tasks");
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(document.getElementById("new-task-description").value));
+    ul.appendChild(li);
+ });
 });
